5e07d7af8f163c667b1d8fbd500be2755f2a8719
ertembiyik <ertembiyik@gmail.com>
2022-05-19 13:20:52 +0300

    gitignore resolve

--- Sources/Ethereum/SmartContract/ERC20.swift
+++ Sources/Ethereum/SmartContract/ERC20.swift
@@ -1,298 +1,199 @@
 import Foundation
 import BigInt
 
-public protocol ERC20Contract {
-    var address: String { get set }
-    func balance(of address: String, completion: @escaping (BigUInt?, Error?) -> ())
-    func transfer(to address: String, amount: BigUInt, gasLimit: BigUInt, gasPrice: BigUInt, with account: Account, completion: @escaping (String?, Error?) -> ())
-    func decimals(completion: @escaping (BigUInt?, Error?) -> ())
-    func symbol(completion: @escaping (String?, Error?) -> ())
-    func name(completion: @escaping (String?, Error?) -> ())
-    func totalSupply(completion: @escaping (BigUInt?, Error?) -> ())
-    func approve(spender: Account, value: BigUInt, completion: @escaping (BigUInt?, Error?) -> ())
-    func deploy(with account: Account, binary: Data, gasLimit: BigUInt, gasPrice: BigUInt, completion: @escaping (String?, Error?) -> ())
+public enum ERC20Contract {
+    
+    case balance(ofAddress: String, contractAddress: String)
+    case transfer(value: BigUInt, to: String, gasLimit: BigUInt, gasPrice: BigUInt, contractAddress: String)
+    case decimals(contractAddress: String)
+    case symbol(contractAddress: String)
+    case name(contractAddress: String)
+    case totalSupply(contractAddress: String)
+    //case deploy
+    
+    // Feature / extension
+    func call(completion: @escaping (Any, Error) -> ()) throws {
+        
+    }
+    
+    public func generateTransaction() throws -> Transaction {
+        switch self {
+            
+        case .balance(let ofAddress, let contractAddress):
+            let params = [SmartContractParam(type: .address, value: EthereumAddress(ofAddress))]
+            
+            let method = SmartContractMethod(name: "balanceOf", params: params)
+            
+            guard let data = method.abiData else {
+                throw ABIError.errorEncodingToABI
+            }
+            
+            return try Transaction(input: data, to: contractAddress)
+            
+        case .transfer(let value, let to, let gasLimit, let gasPrice, let contractAddress):
+            let params = [SmartContractParam(type: .address,  value: EthereumAddress(to)),
+                          SmartContractParam(type: .uint(), value: value)]
+            
+            let method = SmartContractMethod(name: "transfer", params: params)
+            
+            guard let data = method.abiData else {
+                throw ABIError.errorEncodingToABI
+            }
+            
+            return try Transaction(gasLimit: gasLimit, gasPrice: gasPrice, input: data, to: contractAddress, value: BigUInt(0))
+            
+        case .decimals(contractAddress: let contractAddress):
+            let method = SmartContractMethod(name: "decimals", params: [])
+            
+            guard let data = method.abiData else {
+                throw ABIError.errorEncodingToABI
+            }
+            
+            return try Transaction(input: data, to: contractAddress)
+            
+        case .symbol(contractAddress: let contractAddress):
+            let method = SmartContractMethod(name: "symbol", params: [])
+            
+            guard let data = method.abiData else {
+                throw ABIError.errorEncodingToABI
+            }
+            
+            return try Transaction(input: data, to: contractAddress)
+            
+        case .name(contractAddress: let contractAddress):
+            let method = SmartContractMethod(name: "name", params: [])
+            
+            guard let data = method.abiData else {
+                throw ABIError.errorEncodingToABI
+            }
+            
+            return try Transaction(input: data, to: contractAddress)
+            
+        case .totalSupply(contractAddress: let contractAddress):
+            let method = SmartContractMethod(name: "totalSupply", params: [])
+            
+            guard let data = method.abiData else {
+                throw ABIError.errorEncodingToABI
+            }
+            
+            return try Transaction(input: data, to: contractAddress)
+        }
+    }
+    
+    
+    public func decode(output: String) throws -> Any {
+        switch self {
+            
+        case .balance(_, _):
+            let value = output.removeHexPrefix()
+            
+            return try ABIDecoder.decode(value, to: .uint())
+            
+        case .transfer(_, _, _, _, _):
+            return output
+            
+        case .decimals(_):
+            let value = output.removeHexPrefix()
+            
+            return try ABIDecoder.decode(value, to: .uint())
+            
+        case .symbol(_):
+            let value = output.removeHexPrefix()
+            
+            return try ABIDecoder.decode(value, to: .string)
+            
+        case .name(_):
+            let value = output.removeHexPrefix()
+            
+            return try ABIDecoder.decode(value, to: .string)
+            
+        case .totalSupply(_):
+            let value = output.removeHexPrefix()
+            
+            return try ABIDecoder.decode(value, to: .uint())
+        }
+    }
 }
 
-public extension ERC20Contract {
+
+public enum ERC20TransactionFactory {
     
-    func balance(of address: String, completion: @escaping (BigUInt?, Error?) -> ()) {
+    public static func generateBalanceTransaction(address: String, contractAddress: String) throws -> Transaction {
         
         let params = [SmartContractParam(type: .address, value: EthereumAddress(address))]
         
         let method = SmartContractMethod(name: "balanceOf", params: params)
         
         guard let data = method.abiData else {
-            completion(nil, ABIError.errorEncodingToABI)
-            return
-        }
-        
-        var transaction: Transaction
-        
-        do {
-            transaction = try Transaction(input: data, to: self.address)
-        } catch {
-            completion(nil, error)
-            return
-        }
-        
-        EthereumService.call(transaction: transaction) { hexValue, error in
-            
-            guard let hexValue = hexValue, error == nil else {
-                completion(nil, error)
-                return
-            }
-            
-            let value = hexValue.removeHexPrefix()
-            
-            guard let bigUIntValue = try? ABIDecoder.decode(value, to: .uint()) as? BigUInt else {
-                completion(nil, ABIError.errorDecodingFromABI)
-                return
-            }
-            
-            completion(bigUIntValue, nil)
-        }
-        
-    }
-    
-    func transfer(to address: String, amount: BigUInt, gasLimit: BigUInt, gasPrice: BigUInt, with account: Account, completion: @escaping (String?, Error?) -> ()) {
-        
-        let params = [SmartContractParam(type: .address,  value: EthereumAddress(address)),
-                      SmartContractParam(type: .uint(), value: amount)]
-        
-        let method = SmartContractMethod(name: "transfer", params: params)
-        
-        guard let data = method.abiData else {
-            completion(nil, ABIError.errorEncodingToABI)
-            return
-        }
-        
-        var transaction: Transaction
-        
-        do {
-            transaction = try Transaction(gasLimit: gasLimit, gasPrice: gasPrice, input: data, to: self.address, value: amount)
-        } catch {
-            completion(nil, error)
-            return
-        }
-        
-        EthereumService.sendRawTransaction(account: account, transaction: transaction) { hash, error in
-            completion(hash, error)
-        }
-    }
-    
-    func decimals(completion: @escaping (BigUInt?, Error?) -> ()) {
-        
-        let method = SmartContractMethod(name: "decimals", params: [])
-        
-        guard let data = method.abiData else {
-            completion(nil, ABIError.errorEncodingToABI)
-            return
-        }
-        
-        var transaction: Transaction
-        
-        do {
-            transaction = try Transaction(input: data, to: self.address)
-        } catch {
-            completion(nil, error)
-            return
-        }
-        
-        EthereumService.call(transaction: transaction) { hexValue, error in
-            
-            guard let hexValue = hexValue, error == nil else {
-                completion(nil, error)
-                return
-            }
-            
-            let value = hexValue.removeHexPrefix()
-            
-            guard let bigUIntValue = try? ABIDecoder.decode(value, to: .uint()) as? BigUInt else {
-                completion(nil, ABIError.errorDecodingFromABI)
-                return
-            }
-            
-            completion(bigUIntValue, nil)
-        }
-    }
-    
-    func symbol(completion: @escaping (String?, Error?) -> ()) {
-        
-        let method = SmartContractMethod(name: "symbol", params: [])
-        
-        guard let data = method.abiData else {
-            completion(nil, ABIError.errorEncodingToABI)
-            return
-        }
-        
-        var transaction: Transaction
-        
-        do {
-            transaction = try Transaction(input: data, to: self.address)
-        } catch {
-            completion(nil, error)
-            return
-        }
-        
-        EthereumService.call(transaction: transaction) { abiSymbol, error in
-            
-            guard let abiSymbol = abiSymbol, error == nil else {
-                completion(nil, error)
-                return
-            }
-            
-            guard let symbol = try? ABIDecoder.decode(abiSymbol.removeHexPrefix(), to: .string) as? String else {
-                completion(nil, ABIError.errorDecodingFromABI)
-                return
-            }
-            
-            completion(symbol, nil)
-        }
-    }
-    
-    func name(completion: @escaping (String?, Error?) -> ()) {
-        
-        let method = SmartContractMethod(name: "name", params: [])
-        
-        guard let data = method.abiData else {
-            completion(nil, ABIError.errorEncodingToABI)
-            return
-        }
-        
-        var transaction: Transaction
-        
-        do {
-            transaction = try Transaction(input: data, to: self.address)
-        } catch {
-            completion(nil, error)
-            return
-        }
-        
-        EthereumService.call(transaction: transaction) { abiName, error in
-            
-            guard let abiName = abiName, error == nil else {
-                completion(nil, error)
-                return
-            }
-            
-            guard let name = try? ABIDecoder.decode(abiName.removeHexPrefix(), to: .string) as? String else {
-                completion(nil, ABIError.errorDecodingFromABI)
-                return
-            }
-            
-            completion(name, nil)
-        }
-    }
-    
-    func totalSupply(completion: @escaping (BigUInt?, Error?) -> ()) {
-        
-        let method = SmartContractMethod(name: "totalSupply", params: [])
-        
-        guard let data = method.abiData else {
-            completion(nil, ABIError.errorEncodingToABI)
-            return
-        }
-        
-        var transaction: Transaction
-        
-        do {
-            transaction = try Transaction(input: data, to: self.address)
-        } catch {
-            completion(nil, error)
-            return
-        }
-        
-        EthereumService.call(transaction: transaction) { hexValue, error in
-            
-            guard let hexValue = hexValue, error == nil else {
-                completion(nil, error)
-                return
-            }
-            
-            let value = hexValue.removeHexPrefix()
-            
-            guard let bigUIntValue = try? ABIDecoder.decode(value, to: .uint()) as? BigUInt else {
-                completion(nil, ABIError.errorDecodingFromABI)
-                return
-            }
-            
-            completion(bigUIntValue, nil)
-        }
-    }
-    
-    func deploy(with account: Account, binary: Data, gasLimit: BigUInt, gasPrice: BigUInt, completion: @escaping (String?, Error?) -> ()) {
-        
-        var transaction: Transaction
-        
-        do {
-            transaction = try Transaction(gasLimit: gasLimit, gasPrice: gasPrice, input: binary, to: "0x", value: BigUInt(0))
-        } catch {
-            completion(nil, error)
-            return
-        }
-        
-        EthereumService.sendRawTransaction(account: account, transaction: transaction) { hash, error in
-            
-            completion(hash, error)
-        }
-        
-    }
-    
-    func approve(spender: Account, value: BigUInt, completion: @escaping (BigUInt?, Error?) -> ()) {
-        
-        let params = [SmartContractParam(type: .address, value: EthereumAddress(spender.address)),
-                      SmartContractParam(type: .uint(), value: value)]
-        
-        let method = SmartContractMethod(name: "approve", params: params)
-        
-        guard let data = method.abiData else {
-            completion(nil, ABIError.errorEncodingToABI)
-            return
-        }
-        
-        var transaction: Transaction
-        
-        do {
-            transaction = try Transaction(input: data, to: self.address)
-        } catch {
-            completion(nil, error)
-            return
-        }
-        
-        EthereumService.call(transaction: transaction) { hexValue, error in
-            
-            guard let hexValue = hexValue, error == nil else {
-                completion(nil, error)
-                return
-            }
-            
-            let value = hexValue.removeHexPrefix()
-            
-            guard let bigUIntValue = try? ABIDecoder.decode(value, to: .uint()) as? BigUInt else {
-                completion(nil, ABIError.errorDecodingFromABI)
-                return
-            }
-            
-            completion(bigUIntValue, nil)
-        }
-    }
-    
-}
-
-
-extension ERC20Contract {
-    func transferTransaction(to address: String, amount: BigUInt, gasLimit: BigUInt, gasPrice: BigUInt, with account: Account) throws -> Transaction {
-        
-        let params = [SmartContractParam(type: .address,  value: EthereumAddress(address)),
-                      SmartContractParam(type: .uint(), value: amount)]
-        
-        let method = SmartContractMethod(name: "transfer", params: params)
-        
-        guard let data = method.abiData else {
             throw ABIError.errorEncodingToABI
         }
         
-        return try Transaction(gasLimit: gasLimit, gasPrice: gasPrice, input: data, to: self.address, value: BigUInt(0))
+        return try Transaction(input: data, to: contractAddress)
         
     }
+}
+
+//func deploy(with account: Account, binary: Data, gasLimit: BigUInt, gasPrice: BigUInt, completion: @escaping (String?, Error?) -> ()) {
+//
+//    var transaction: Transaction
+//
+//    do {
+//        transaction = try Transaction(gasLimit: gasLimit, gasPrice: gasPrice, input: binary, to: "0x", value: BigUInt(0))
+//    } catch {
+//        completion(nil, error)
+//        return
+//    }
+//
+//    EthereumService.sendRawTransaction(account: account, transaction: transaction) { hash, error in
+//
+//        completion(hash, error)
+//    }
+//
+//}
+//
+//
+
+//func approve(spender: Account, value: BigUInt, completion: @escaping (BigUInt?, Error?) -> ()) {
+//
+//    let params = [SmartContractParam(type: .address, value: EthereumAddress(spender.address)),
+//                  SmartContractParam(type: .uint(), value: value)]
+//
+//    let method = SmartContractMethod(name: "approve", params: params)
+//
+//    guard let data = method.abiData else {
+//        completion(nil, ABIError.errorEncodingToABI)
+//        return
+//    }
+//
+//    var transaction: Transaction
+//
+//    do {
+//        transaction = try Transaction(input: data, to: self.address)
+//    } catch {
+//        completion(nil, error)
+//        return
+//    }
+//
+//    EthereumService.call(transaction: transaction) { hexValue, error in
+//
+//        guard let hexValue = hexValue, error == nil else {
+//            completion(nil, error)
+//            return
+//        }
+//
+//        let value = hexValue.removeHexPrefix()
+//
+//        guard let bigUIntValue = try? ABIDecoder.decode(value, to: .uint()) as? BigUInt else {
+//            completion(nil, ABIError.errorDecodingFromABI)
+//            return
+//        }
+//
+//        completion(bigUIntValue, nil)
+//    }
+//}
+
+
+
+enum ERC20ContractError: Error {
+    case invalidTransaction(ERC20Contract)
 }
--- Sources/Ethereum/SmartContract/SmartContract.swift
+++ Sources/Ethereum/SmartContract/SmartContract.swift
@@ -1,20 +1,11 @@
 import Foundation
 import BigInt
 
-protocol SmartContractProtocol {
+public protocol SmartContractProtocol {
     
-    init(abi: String, address: String)
-    
-    // MARK: - write an extension with default realisation
-    func method<T: Codable>(name: String, params: T) -> Transaction
-    
-    var allMethods: [String] { get }
-    
-    var allEvents: [String] { get }
+    var address: String { get set }
     
 }
-
-extension SmartContractProtocol {}
 
 
 
